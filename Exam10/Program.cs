4c1a0c98dea931197229aec870cbbd65e29f4eb6
Nikolay <labazopelastra@gmail.com>
2022-12-04 10:47:54 +0700

    Create a new program

---
+++ Exam10/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите число от 100 до 999 включительно ");
+int num = int.Parse(Console.ReadLine()!);
+if (num <= 99 || num > 999)
+{
+    System.Console.WriteLine(num + " не входит в диапазон программы.Введите число от 100 до 999 включительно");
+}
+int num2 = (num / 10) % 10;
+System.Console.WriteLine("Вторая цифра числа " + num + ": " + num2);
+
+
